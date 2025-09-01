89ce825a12d74886ddbefbf5131b84c88cbd1be8
matildefranco <matilde@craft.do>
2025-09-01 12:19:26 +0200

    Add files via upload

---
+++ first-website/scripts/main.js
@@ -0,0 +1,40 @@
+const listItems = document.querySelectorAll("li");
+
+function toggleDone(e) {
+  if (!e.target.className) {
+    e.target.className = "done";
+  } else {
+    e.target.className = "";
+  }
+}
+
+const myImage = document.querySelector("img");
+
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/newyorksky.png") {
+    myImage.setAttribute("src", "images/dextar.png");
+  } else {
+    myImage.setAttribute("src", "images/newyorksky.png");
+  }
+});
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+myButton.addEventListener("click", () => {
+  setUserName();
+});
